32c49650b60b7ceab52d16a4d0509a0e2b9caefa
Venky <venkatramanm@gmail.com>
2025-10-21 00:33:21 +0530

    Just save minor fixes

--- src/main/java/in/succinct/bpp/shopify/extensions/ListingWebhook.java
+++ src/main/java/in/succinct/bpp/shopify/extensions/ListingWebhook.java
@@ -69,7 +69,7 @@ public class ListingWebhook extends ShopifyWebhook {
         context.setNetworkId(networkAdaptor.getId());
         context.setCity(adaptor.getSubscriber().getCity());
         context.setAction("on_search");
-        context.setTtl(60);
+        context.setTtl(60L);
         for (in.succinct.beckn.Provider provider : providers){
             provider.setTag("general_attributes","catalog.indexer.reset","N");
             provider.setTag("general_attributes","catalog.indexer.operation",operation.name());
